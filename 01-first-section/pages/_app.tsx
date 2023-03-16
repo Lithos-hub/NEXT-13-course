5e2670b11c25e3cdffb6c33e44fa1236ae3b732f
Carlos Segura <lithos.contact@gmail.com>
2023-03-16 09:40:19 +0100

    remove section folders and collapse into one

--- 01-first-section/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-import { NextComponentType } from "next";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout || ((page: JSX.Element) => page);
-
-  return getLayout(<Component {...pageProps} />);
-}
